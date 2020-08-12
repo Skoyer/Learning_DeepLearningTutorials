088907a6274e279e6b35f174b22a53b2214eb42c
Tim <lhokie@vt.edu>
2020-08-12 14:06:53 -0400

    Mostly AI Cohort 2020.

---
+++ ai_cohort/Introduction_Basic_DeepLearning/002_style_Transfer_picture_conversion/styleTransfer/temp.bat
@@ -0,0 +1,6 @@
+python evaluate.py --checkpoint .\checkpoints\rain-princess.ckpt --in-path .\tim_photos\dale.jpg --out-path .\out-photos\dale_01.jpg
+python evaluate.py --checkpoint .\checkpoints\la-muse.ckpt --in-path .\tim_photos\dale.jpg --out-path .\out-photos\dale_02.jpg
+python evaluate.py --checkpoint .\checkpoints\scream.ckpt --in-path .\tim_photos\dale.jpg --out-path .\out-photos\dale_03.jpg
+python evaluate.py --checkpoint .\checkpoints\udnie.ckpt --in-path .\tim_photos\dale.jpg --out-path .\out-photos\dale_04.jpg
+python evaluate.py --checkpoint .\checkpoints\wave.ckpt --in-path .\tim_photos\dale.jpg --out-path .\out-photos\dale_05.jpg
+python evaluate.py --checkpoint .\checkpoints\wreck.ckpt --in-path .\tim_photos\dale.jpg --out-path .\out-photos\dale_06.jpg
