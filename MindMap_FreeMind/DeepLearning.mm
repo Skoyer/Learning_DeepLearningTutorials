844d8fd25dc3144d781cb7b3c0d5d87aa8b26b02
Tim <lhokie@vt.edu>
2020-08-14 12:23:24 -0400

    Cleaned up a lot of stuff with the old files from before the bicycle project. Added the work on NLP with Andrew Trask.

---
+++ MindMap_FreeMind/DeepLearning.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1597327027381" ID="ID_584779816" MODIFIED="1597327045203" TEXT="DeepLearning">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1597327121528" ID="ID_1528841150" MODIFIED="1597327125880" POSITION="right" TEXT="Reference">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1597327331199" ID="ID_899233890" MODIFIED="1597327338376" POSITION="right" TEXT="Introduction to Deep Learning">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1597327343845" ID="ID_819213359" MODIFIED="1597327353328" POSITION="left" TEXT="2. Neural Networks">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1597327267525" ID="ID_1626313486" MODIFIED="1597327356820" TEXT="Lesson 6: Sentinment Analysis">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1597327364434" ID="ID_313782396" MODIFIED="1597327414815" TEXT="24 Chapters - Andrew Trask">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
