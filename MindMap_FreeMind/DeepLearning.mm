363f9a2ee16aa64d60b3487675edb938fc8a8af4
Tim <lhokie@vt.edu>
2020-08-15 12:08:06 -0400

    Added the Andrew Trask notes from another website - great tutorial. Need to go back and find the youtube examples that go over this, but it was fascinating to see how he mangaged and updated the neural network for NLP.
    Read Grokking Deep Learning 5-8 for more details.

--- MindMap_FreeMind/DeepLearning.mm
+++ MindMap_FreeMind/DeepLearning.mm
@@ -19,6 +19,17 @@
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1597327364434" ID="ID_313782396" MODIFIED="1597327414815" TEXT="24 Chapters - Andrew Trask">
 <font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1597422531914" ID="ID_1833003727" MODIFIED="1597422539326" TEXT="Looked at the data"/>
+<node COLOR="#111111" CREATED="1597422541825" ID="ID_392778570" MODIFIED="1597422550913" TEXT="Try to do the same thing with another similar data set"/>
+<node COLOR="#111111" CREATED="1597422582038" ID="ID_686671088" MODIFIED="1597422599201" TEXT="Try a faster speed for reviews - 1.75x"/>
+</node>
+<node COLOR="#990000" CREATED="1597422672902" ID="ID_1645939704" MODIFIED="1597422683445" TEXT="Things I might Like to play with">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1597422685114" ID="ID_890419701" MODIFIED="1597422692358" TEXT="Using the collections counter">
+<node COLOR="#111111" CREATED="1597423621173" ID="ID_399953757" MODIFIED="1597423627801" TEXT="Needed to be installed separately"/>
+</node>
+<node COLOR="#111111" CREATED="1597422704313" ID="ID_822398754" MODIFIED="1597422712670" TEXT="Create dictionary and do something with it"/>
+<node COLOR="#111111" CREATED="1597422757146" ID="ID_543645626" MODIFIED="1597422781889" TEXT="Positive counts most negative positive_counts.most_common()"/>
 </node>
 </node>
 </node>
