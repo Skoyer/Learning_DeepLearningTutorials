0e01bc6873172783717032470a5e609ad2ada8be
Tim <lhokie@vt.edu>
2020-08-30 11:01:29 -0400

    Not sure - but added stuff for CNN I expect - tensors and pytorch.

---
+++ MindMap_FreeMind/8-29 Study.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1598714460104" ID="ID_373315396" MODIFIED="1598714471952" TEXT="8-29 Study"/>
+</map>
